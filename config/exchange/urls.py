b5e7648660b20202279dd054c2739e870dee746f
hughpythoneer <hughpythoneer@gmail.com>
2021-06-16 17:33:45 -0700

    updates

--- config/exchange/urls.py
+++ config/exchange/urls.py
@@ -4,8 +4,8 @@ from .views import *
 
 urlpatterns = [
 	path('', IndexView.as_view(), name='index'),
-    #path('request/token', TokenView.as_view(), name='token'),
     path('request/token', RequestTokenView.as_view(), name='request-token'),
     path('status/token', StatusTokenView.as_view(), name='status-token'),
-    path('exchange/api', ApiRestView.as_view(), name='api-rest')
+    path('exchange/api', ApiRestView.as_view(), name='api-rest'),
+    path('api/authtoken/token=<str:token>&base=USD&change=MXN', EnpointView.as_view(), name='enpoint-rest-api')
 ]
--- config/exchange/views.py
+++ config/exchange/views.py
@@ -1,4 +1,5 @@
 from django.http.response import HttpResponse
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.views.generic import View
 import pandas as pd
@@ -61,7 +62,7 @@ class ApiRestView(View):
             
             if token == fungible_token:
                 """Update token life"""
-                update = TokenModel.objects.filter(token=token).update(life=life-1)
+                #update = TokenModel.objects.filter(token=token).update(life=life-1)
 
                 """Api Rest Fixer """
                 api = pd.read_json('http://data.fixer.io/api/latest?access_key=8a3b70c7922a9348614d7708f45b3427&symbols=MXN')
@@ -82,7 +83,7 @@ class ApiRestView(View):
                 value_banxico_page = df.at[2, 3]
 
 
-                # Api Rest Banxico
+                """ Api Rest Banxico"""
                 token= 'd906a65284a766c522a100057936491f924d7692941d5c5937f7671fb6e7da6e'
                 url = 'https://www.banxico.org.mx/SieAPIRest/service/v1/series/SF43718/datos/oportuno?token=%s'% token
                 data = requests.get(url)
@@ -115,14 +116,15 @@ class ApiRestView(View):
                     }
             
                 #context = {'response':format}
-                context = {'table':'Rates:','date_fixer_api':date_fixer_api,'value_fixer_api':value_fixer_api,'date_banxico_page':date_banxico_page,'value_banxico_page':value_banxico_page,'date_banxico_api':date_banxico_api,'value_banxico_api':value_banxico_api}
+                context = {'table':'Rates:','authtoken':token,'date_fixer_api':date_fixer_api,'value_fixer_api':value_fixer_api,'date_banxico_page':date_banxico_page,'value_banxico_page':value_banxico_page,'date_banxico_api':date_banxico_api,'value_banxico_api':value_banxico_api}
                 return render(request, self.template_name, context)
+            else:
+                context = {'message':'Error in response'}
+                return render(request, self.template_name, context)
+
         context = {'message':'Error in response'}
         return render(request, self.template_name, context)
     
-
-
-
 
 class StatusTokenView(View):
     """docstring for StatusTokenView"""
@@ -142,3 +144,87 @@ class StatusTokenView(View):
         context = {'message': 'Error in response'}
         return render(request, self.template_name)
 
+
+class EnpointView(View):
+    """docstring for EnpointView"""
+    template_name = 'template.starter.html'
+    def get(self, request, token):
+        """Update token life"""
+        queryset = TokenModel.objects.filter(token=token)
+        for i in queryset:
+            life = i.life
+        
+            update = TokenModel.objects.filter(token=token).update(life=life-1)
+
+        """Api Rest Fixer """
+        api = pd.read_json('http://data.fixer.io/api/latest?access_key=8a3b70c7922a9348614d7708f45b3427&symbols=MXN')
+        df = pd.DataFrame(api)
+        date_fixer_api = df.date
+        date_fixer_api = date_fixer_api[0].date().strftime("%d/%m/%y")
+        value_fixer_api = df.rates
+        value_fixer_api = value_fixer_api.values[0]
+
+        """ Official Journal of the Federation https://www.banxico.org.mx/tipcamb/tipCamMIAction.do """
+
+        df = pd.read_html('https://www.banxico.org.mx/tipcamb/tipCamMIAction.do')
+        df_values = df[6].values
+        df = pd.DataFrame(data=df_values)
+        df = df.dropna()
+        df = df.loc[0:2]
+        date_banxico_page = df.at[2, 0]
+        value_banxico_page = df.at[2, 3]
+
+
+        """ Api Rest Banxico"""
+        token= 'd906a65284a766c522a100057936491f924d7692941d5c5937f7671fb6e7da6e'
+        url = 'https://www.banxico.org.mx/SieAPIRest/service/v1/series/SF43718/datos/oportuno?token=%s'% token
+        data = requests.get(url)
+        dict_j  = data.json()
+        for value in dict_j.values():
+            for v in value.values():
+                for k in v:
+                    info = k['datos']
+                    data = dict(info[0])
+                    value_banxico_api = data['dato']
+                    date_banxico_api = data['fecha']
+
+
+            
+            format = {
+                "rates": {
+                    'provider_1':{
+                        'last_update':date_fixer_api,
+                        'value': value_fixer_api,
+                    },
+                    'provider_2_variant_1':{
+                        'last_update': date_banxico_page,
+                        'value': value_banxico_page,
+                    },
+                    'provider_2_variant_2':{
+                        'last_update': date_banxico_api,
+                        'value': value_fixer_api,
+                    }
+                }
+            }
+        
+            import json
+            data = {}
+            data['Rates'] = []
+            data['Rates'].append({
+                'provider': '1',
+                'variant': 'None',
+                'last_update': date_fixer_api,
+                'value': value_fixer_api})
+            data['Rates'].append({
+                'provider': '2',
+                'variant': '1',
+                'last_update': date_banxico_page,
+                'value': value_banxico_page})
+            data['Rates'].append({
+                'provider': '2',
+                'variant': '2',
+                'last_update': date_banxico_api,
+                'value': value_fixer_api})
+        response = JsonResponse(data)
+        content = response.content
+        return HttpResponse(content)
